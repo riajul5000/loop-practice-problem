668eb5a5f03626d2738fa4bdddeccdee9a6bd908
riajul5000 <riyazulislam500@gmail.com>
2023-12-12 21:44:08 +0100

    something add js code

---
+++ average.js
@@ -0,0 +1,16 @@
+var math = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla= 99.50;
+var totalRate = math + biology + chemistry + physics + bangla;
+// console.log(totalRate);
+var average = totalRate / 5;
+clearAverage = average.toFixed(2);
+clearAverageTo = parseFloat(clearAverage);
+console.log(clearAverageTo);
+
+var sarahMother = 119;
+var sarahDived = 5;
+var sarahNumber = sarahMother % sarahDived;
+console.log(sarahNumber);
--- revison.js
+++ revison.js
@@ -21,4 +21,27 @@ var penIndex = tableItems.indexOf('pen');
 var moneyGiven = 1000;
 var orangeApplePrice = 700;
 var totalRest = moneyGiven - orangeApplePrice;
-console.log(totalRest); 
+// console.log(totalRest); 
+
+var fruits = ['Apple', 'banana', 'orange'];
+fruits[1] = 'Mango';
+fruits.pop();
+fruits.push('Watermelon');
+// console.log(fruits);
+
+var num1 = 113;
+var num2 = 79;
+var num3 = 45;
+ if(num1 > num2){
+    console.log(num1);
+    if(num2 > num1){
+        console.log(num2);
+    }else{
+        console.log(num1);
+    }
+ }
+ else{
+    if(num2 > num3){
+        console.log(num2)
+    }
+ }
